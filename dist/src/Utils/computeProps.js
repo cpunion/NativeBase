587caa0b9d3164facfa59f548ca4025d38e282e0
Li Jie <lijie@applean.cn>
2017-09-29 15:16:41 +0800

    Build

--- dist/src/Utils/computeProps.js
+++ dist/src/Utils/computeProps.js
@@ -1,8 +1,5 @@
 "use_strict";
-var _lodash=require("lodash");var _lodash2=_interopRequireDefault(_lodash);
-var _ReactNativePropRegistry=require("react-native/Libraries/Renderer/shims/ReactNativePropRegistry");var _ReactNativePropRegistry2=_interopRequireDefault(_ReactNativePropRegistry);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
-
-
+var _lodash=require("lodash");var _lodash2=_interopRequireDefault(_lodash);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
 module.exports=function(incomingProps,defaultProps){
 
 var computedProps={};
@@ -21,23 +18,11 @@ computedProps=defaultProps;
 }
 
 if(incomingPropsStyle){
-var computedPropsStyle={};
-computedProps.style={};
-if(Array.isArray(incomingPropsStyle)){
-_lodash2.default.forEach(incomingPropsStyle,function(style){
-if(typeof style==="number"){
-_lodash2.default.merge(computedPropsStyle,_ReactNativePropRegistry2.default.getByID(style));
+if(computedProps.style){
+computedProps.style=[computedProps.style,incomingPropsStyle];
 }else{
-_lodash2.default.merge(computedPropsStyle,style);
+computedProps.style=incomingPropsStyle;
 }
-});
-}else if(typeof incomingPropsStyle==="number"){
-computedPropsStyle=_ReactNativePropRegistry2.default.getByID(incomingPropsStyle);
-}else{
-computedPropsStyle=incomingPropsStyle;
-}
-
-_lodash2.default.merge(computedProps.style,defaultProps.style,computedPropsStyle);
 }
 
 return computedProps;
